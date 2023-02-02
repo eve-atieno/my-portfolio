4452dec32c413482bf484e40020e42dd257e2979
eve-atieno <atienoevelyne53@gmail.com>
2023-02-02 14:09:37 +0300

    updated index.css file

--- portfolio/src/ components/Home.js
+++ portfolio/src/ components/Home.js
@@ -3,17 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <section class="home" id="home">
-        
-        <div class="max-width">
-            <div class="home-content">
-                <div class="text-1">Hello, my name is</div>
-                <div class="text-2">Evelyne Atieno</div>
-                <div class="text-3">And I'm <span class="typing"></span></div>
-                <a href="#contact">Contact Me</a>
-            </div>
-        </div>
-    </section>
+      
     </div>
   )
 }
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
-import NavHome from './ components/NavHome';
-import About from './ components/About';
-import Contact from './ components/Contact';
-// import {Routes,Route,} from 'react-router-dom';
-import Services from './ components/Services';
-import Skills from './ components/Skills';
+
 
 
 function App() {
   return (
     <div className="App">
-    < NavHome/>
-      {/* <About/>
-      <Services/>
-      <Skills/>
-      <Contact/>
-       */}
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
